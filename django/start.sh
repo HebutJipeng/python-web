157e738c8804b9e9046bd49be59eb76fd66c00e8
jipeng <jipeng@gotokeep.com>
2018-07-28 00:53:00 +0800

    init project

---
+++ django/start.sh
@@ -0,0 +1,10 @@
+python3 manage.py runserver 0.0.0.0:8000
+
+# 如果出现下面的报错
+# You have 14 unapplied migration(s). Your project may not work properly until you apply the migrations for app(s): admin, auth, contenttypes, sessions.
+# Run 'python manage.py migrate' to apply them.
+# 运行 python manage.py migrate 来fix它
+
+# superadmin 
+# account:  jipeng
+# password: jp123456
